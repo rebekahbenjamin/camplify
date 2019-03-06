208a00f65533c1032732b483f25e6d772806ea6a
Rebekah Benjamin <rebekah.benjamin@gmail.com>
2019-03-06 08:49:22 +0000

    master : handle none booked_events

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -9,45 +9,66 @@ class EventService
   end
 
   def self.find(year: year, week: week)
-    booked_events = Event.where(
+    events = Event.where(
       year: year,
       week_number: week
-    ).sort_by { |e| e.start_date }
+    )
     available_slot = []
     next_avail = nil
-    booked_events.each do |event|
-      start_date = DateTime.parse(event.start_date.to_s)
-      end_date = DateTime.parse(event.end_date.to_s)
-      if (start_date > start_date.midnight) || next_avail < start_date
-        if next_avail.present? && next_avail.day < start_date.day
-          slot = {
-            start_date: next_avail,
-            end_date: end_date.end_of_day
-          }
-          available_slot << slot
-          next_avail = nil
-        end
+    if events.any?
+      booked_events = events.sort_by { |e| e.start_date }
 
-        if next_avail.present? && next_avail.day == start_date.day
-          slot =  {
-            start_date: next_avail,
-            end_date: start_date
-          }
-        else
-          slot = {
-            start_date: start_date.midnight,
-            end_date: start_date
-          }
+      booked_events.each do |event|
+        start_date = DateTime.parse(event.start_date.to_s)
+        end_date = DateTime.parse(event.end_date.to_s)
+        if (start_date > start_date.midnight) || next_avail < start_date
+          if next_avail.present? && next_avail.day < start_date.day
+            slot = {
+              start_date: next_avail,
+              end_date: end_date.end_of_day
+            }
+            available_slot << slot
+            next_avail = nil
+          end
+
+          if next_avail.present? && next_avail.day == start_date.day
+            slot =  {
+              start_date: next_avail,
+              end_date: start_date
+            }
+          else
+            slot = {
+              start_date: start_date.midnight,
+              end_date: start_date
+            }
+          end
+          available_slot << slot
+          next_avail = event.end_date
         end
-        available_slot << slot
-        next_avail = event.end_date
       end
+      if booked_events.last[:end_date] <= next_avail.end_of_day
+        available_slot << {
+          start_time: next_avail,
+          end_date: booked_events.last[:end_date].end_of_day
+        }
+      end
+    else
+      available_slot = fetch_full_empty_slot(week, year)
     end
-    if booked_events.last[:end_date] <= next_avail.end_of_day
-      available_slot << {
-        start_time: next_avail,
-        end_date: booked_events.last[:end_date].end_of_day
+    available_slot
+  end
+
+  def self.fetch_full_empty_slot(week, year)
+    available_slot = []
+    starting_date = Date.commercial(year, week)
+    7.times do
+      first_day = starting_date
+      slot = {
+        start_day: starting_date.midnight,
+        end_date: starting_date.end_of_day
       }
+      available_slot << slot
+      starting_date = starting_date.next_day
     end
     available_slot
   end
