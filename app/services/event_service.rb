97b1db5b8df67dc34133de3d2a76d0b73b1fc911
Rebekah Benjamin <rebekah.benjamin@gmail.com>
2019-03-06 11:18:53 +0000

    master : available slot

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -16,42 +16,7 @@ class EventService
     available_slot = []
     next_avail = nil
     if events.any?
-      booked_events = events.sort_by { |e| e.start_date }
-
-      booked_events.each do |event|
-        start_date = DateTime.parse(event.start_date.to_s)
-        end_date = DateTime.parse(event.end_date.to_s)
-        if (start_date > start_date.midnight) || next_avail < start_date
-          if next_avail.present? && next_avail.day < start_date.day
-            slot = {
-              start_date: next_avail,
-              end_date: end_date.end_of_day
-            }
-            available_slot << slot
-            next_avail = nil
-          end
-
-          if next_avail.present? && next_avail.day == start_date.day
-            slot =  {
-              start_date: next_avail,
-              end_date: start_date
-            }
-          else
-            slot = {
-              start_date: start_date.midnight,
-              end_date: start_date
-            }
-          end
-          available_slot << slot
-          next_avail = event.end_date
-        end
-      end
-      if booked_events.last[:end_date] <= next_avail.end_of_day
-        available_slot << {
-          start_time: next_avail,
-          end_date: booked_events.last[:end_date].end_of_day
-        }
-      end
+      available_slot = get_available_slots(events, week, year)
     else
       available_slot = fetch_full_empty_slot(week, year)
     end
@@ -62,12 +27,62 @@ class EventService
     available_slot = []
     starting_date = Date.commercial(year, week)
     7.times do
-      first_day = starting_date
       slot = {
         start_day: starting_date.midnight,
         end_date: starting_date.end_of_day
       }
       available_slot << slot
+      starting_date = starting_date.next_day
+    end
+    available_slot
+  end
+
+  def self.get_available_slots(events, week, year)
+    available_slot = []
+    booked_events = events.sort_by { |e| e.start_date }
+    starting_date = Date.commercial(year, week)
+    7.times do
+      booked_slots = booked_events.select do |i|
+        Date.parse(i[:start_date].to_s) == starting_date
+      end
+      if booked_slots.any?
+        next_avail = nil
+        booked_slots.each do |slot|
+          start_date = DateTime.parse(slot.start_date.to_s)
+          end_date = DateTime.parse(slot.end_date.to_s)
+          slot = nil
+          if next_avail.nil?
+            slot = {
+              start_date: starting_date.midnight,
+              end_date: start_date
+            }
+            next_avail = end_date
+          else
+            slot = {
+              start_date: next_avail,
+              end_date: start_date
+            }
+            next_avail = end_date
+          end
+          available_slot << slot
+        end
+        last_slot = booked_slots.last
+        final_end_date = last_slot[:end_date]
+        end_of_date = DateTime.parse(starting_date.end_of_day.to_s)
+        if final_end_date != end_of_date
+          final_slot = {
+            start_date: last_slot[:end_date],
+            end_date: end_of_date
+          }
+          available_slot << final_slot
+        end
+      else
+        slot = {
+          start_date: starting_date.midnight,
+          end_date: starting_date.end_of_day
+        }
+        available_slot << slot
+      end
       starting_date = starting_date.next_day
     end
     available_slot
